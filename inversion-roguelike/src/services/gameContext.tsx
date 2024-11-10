d39a09923f54c990ef12736536a1ff0654a529ad
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-11-10 19:29:11 +0100

    Add GameContextProvider and useTime hook for game state management

--- inversion-roguelike/src/services/gameContext.tsx
+++ inversion-roguelike/src/services/gameContext.tsx
@@ -1,25 +1,40 @@
+"use client";
 import { usePlayer } from "@/hooks/usePlayer";
-import { useMemo } from "react";
-import { createContext } from "vm";
+import { useTime } from "@/hooks/useTime";
+import { createContext, useContext, useMemo } from "react";
 
-const GameContext = createContext({
+interface ContextInterface {
+  player: ReturnType<typeof usePlayer>;
+  time: ReturnType<typeof useTime>;
+}
+
+const GameContext = createContext<ContextInterface>({
   player: null as unknown as ReturnType<typeof usePlayer>,
+  time: null as unknown as ReturnType<typeof useTime>,
 });
 
-export function GlobalContextProvider({
+export function GameContextProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const player = usePlayer();
-
+  const time = useTime();
   const memo = useMemo(() => {
     return {
       player,
+      time,
     };
   }, [player]);
 
-  return (
-    <GameContext.Provider value={{ memo }}>{children}</GameContext.Provider>
-  );
+  return <GameContext.Provider value={memo}>{children}</GameContext.Provider>;
+}
+
+export function useGameContext() {
+  const contex = useContext(GameContext);
+
+  if (contex === undefined) {
+    throw new Error("useGlobalContext must be used within a GlobalProvider");
+  }
+  return contex;
 }
